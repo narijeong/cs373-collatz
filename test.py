00495ce2e66ae9a90f8466ddcb0ad39f77b11321
Nari Jeong <nari@wireless-10-147-187-197.public.utexas.edu>
2015-06-17 18:41:04 -0500

    cover the whole range in meta cache in Caollatz.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Code to make meta cache in range of 1000 in 1-100000
 from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
 
-for x in range(1, 100000, 1000) :
+for x in range(1, 1000001, 1000) :
 	max = collatz_eval(x, x+999)
 	#print(x, x+49) 
 	print(max,",",end=" ")
